f58c6c5afd63d403405f9e196eb0940844a5ec23
hayato.setoguchi <hayaman0615@gmail.com>
2021-04-27 12:18:21 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,9 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
 
   def index
-    @tasks = Task.all
+    #@tasks = Task.all
+    @task = current_user.tasks.build
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
@@ -14,7 +16,9 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
+    
 
     if @task.save
       flash[:success] = 'タスク が正常に作成されました'
