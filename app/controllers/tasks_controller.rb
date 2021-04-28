91bad69bd49c37059b39c4a3940eb5912408100e
hayato.setoguchi <hayaman0615@gmail.com>
2021-04-28 09:03:06 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :destroy]
   before_action :require_user_logged_in, only: [:index, :show, :edit, :destroy]
   
@@ -53,9 +52,7 @@ class TasksController < ApplicationController
 
   private
 
-  def set_task
-    @task = Task.find(params[:id])
-  end
+  
 
   def task_params
     params.require(:task).permit(:content, :status)
@@ -64,7 +61,7 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to tasks_url
+      redirect_to login_url
     end
   end
 end
